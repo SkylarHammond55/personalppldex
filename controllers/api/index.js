fc2ca9e499b3e18730aeb076cd701cfe04e744bf
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-08-12 16:42:32 -0500

    Got the routes to work for get all for user information

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const profileRoutes = require('./profileRoutes');
+// const profileRoutes = require('./profileRoutes');
 
 router.use('/users', userRoutes);
-router.use('/profiles', profileRoutes);
+// router.use('/profiles', profileRoutes);
 
 module.exports = router;
