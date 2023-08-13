2081a0a2ad75c50696a034426a8fe7ea9852d68e
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-08-12 20:18:12 -0500

    Login route works, need to resolve a way to have both login and signup on the same page within one route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,6 +15,7 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+    // Sign-up Route - Creates a new user (Works)
     try {
         const userData = await User.create(req.body);
 
@@ -25,9 +26,44 @@ router.post('/', async (req, res) => {
             res.status(200).json(userData);
         });
     } catch (err) {
-        res.status(400).json(err)
+        res.status(400).json(err);
     }
-})
+});
+
+// Login-Route - Logins existing user (Works)
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: { username: req.body.username }
+        })
+
+        if (!userData) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect username or password, please try again.'});
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect username or password, please try again.'});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: "You are now logged in!"});
+        })
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 
 // router.get('/', async (req, res) => {
 //     try {
