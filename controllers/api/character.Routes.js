b52df4ce344fece1eea4504251d39dca96c7d899
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-08-12 20:37:29 -0500

    Added a new file called characterRoutes.js to handle the create a character page. Moved the post route from dashboardRoutes.js to characterRoutes.js

---
+++ controllers/api/character.Routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Profile } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newProfile = await Profile.create({
+        ...req.body,
+        user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newProfile);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -2,18 +2,18 @@ const router = require('express').Router();
 const { Profile } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
-    try {
-        const newProfile = await Profile.create({
-        ...req.body,
-        user_id: req.session.user_id,
-        });
+// router.post('/', withAuth, async (req, res) => {
+//     try {
+//         const newProfile = await Profile.create({
+//         ...req.body,
+//         user_id: req.session.user_id,
+//         });
 
-        res.status(200).json(newProfile);
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
+//         res.status(200).json(newProfile);
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
 
 router.delete('/:id', withAuth, async (req, res) => {
     try {
