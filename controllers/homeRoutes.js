60e3b3dc255129a3abc01846674242a25ee659bd
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-08-13 21:00:00 -0500

    Added logout functionality by updating logout.js with a logout function, updating route to be 'post' under userRoutes, and commented out the login/signup form on the homepage for the time being

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,58 +12,16 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
     // Add a comment describing the purpose of the render method
     // This method is rendering the 'homepage' handlebars.js template. This is how we connect each route to the correct template.
-    res.render('homepage');
+    res.render('homepage', {
+        logged_in: req.session.logged_in,
+    });
 });
 
-// // Sign-up Route - Creates a new user (Works)
-// router.post('/signup', async (req, res) => {
-//     try {
-//         const userData = await User.create(req.body);
-
-//         req.session.save(() => {
-//             req.session.user_id = userData.id;
-//             req.session.logged_in = true;
-
-//             res.status(200).json(userData);
-//         });
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
-
-// // Login-Route - Logins existing user (Works)
-// router.post('/login', async (req, res) => {
-//     try {
-//         const userData = await User.findOne({
-//             where: { username: req.body.username }
-//         })
-
-//         if (!userData) {
-//             res
-//             .status(400)
-//             .json({ message: 'Incorrect username or password, please try again.'});
-//             return;
-//         }
-
-//         const validPassword = await userData.checkPassword(req.body.password);
-
-//         if (!validPassword) {
-//             res
-//             .status(400)
-//             .json({ message: 'Incorrect username or password, please try again.'});
-//             return;
-//         }
-
-//         req.session.save(() => {
-//             req.session.user_id = userData.id;
-//             req.session.logged_in = true;
-
-//             res.json({ user: userData, message: "You are now logged in!"});
-//         })
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
+router.get('/login', async (req, res) => {
+    res.render('login', {
+        logged_in: req.session.logged_in,
+    })
+})
 
 // Gets User based on id and includes profile data for specific user
 router.get('/dashboard', withAuth, async (req, res) => {
@@ -96,7 +54,6 @@ router.get('/login', (req, res) => {
         res.redirect('/');
         return;
     }
-
     res.render('login');
 });
 
