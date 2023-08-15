b71a678c985683ed05ff70bce8e907f8ff1b66a4
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-08-15 16:58:49 -0500

    styling changes

--- seeds/profileData.js
+++ seeds/profileData.js
@@ -11,7 +11,7 @@ const profiledata = [
     },
     {
         name: "Hermione Granger",
-        image: "imagetest",
+        image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*obDCz57X7D2Kd1XvcdI0Iw.jpeg",
         bio: "Hermione Granger is a muggle-born wizard who is currently attending Hogwarts. She is good friends with both Ron Weasley and Harry Potter.",
         user_id: 1,
         skill_id: [1, 2, 4, 10, 11],
