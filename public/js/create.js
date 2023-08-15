e255a8a68b95c1b81e67be5b16ffc6a7e954be6f
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-08-15 12:57:59 -0500

    Downloaded Multer and began working on create.js to test to functionality

--- public/js/create.js
+++ public/js/create.js
@@ -1,34 +1,45 @@
-const data = {
-  //  will fix later to an actual dropdown
+// const data = {
+//   //  will fix later to an actual dropdown
 
-  alignments: [
-    "Lawful Good",
-    "Neutral Good",
-    "Chaotic Good",
-    "Lawful Neutral",
-    "True Neutral",
-    "Chaotic Neutral",
-    "Lawful Evil",
-    "Neutral Evil",
-    "Chaotic Evil",
-  ],
-  // will fix later
-  stats: [{ name: "Stat1", options: ["Option1", "Option2"] }],
-  abilities: ["Ability 1", "Ability 2", "Ability 3", "Ability 4"],
-  skills: ["Skill 1", "Skill 2", "Skill 3", "Skill 4"],
-};
+//   alignments: [
+//     "Lawful Good",
+//     "Neutral Good",
+//     "Chaotic Good",
+//     "Lawful Neutral",
+//     "True Neutral",
+//     "Chaotic Neutral",
+//     "Lawful Evil",
+//     "Neutral Evil",
+//     "Chaotic Evil",
+//   ],
+//   // will fix later
+//   stats: [{ name: "Stat1", options: ["Option1", "Option2"] }],
+//   abilities: ["Ability 1", "Ability 2", "Ability 3", "Ability 4"],
+//   skills: ["Skill 1", "Skill 2", "Skill 3", "Skill 4"],
+// };
 
-const source = document.getElementById("character-template").innerHTML;
-const template = Handlebars.compile(source);
+// const source = document.getElementById("character-template").innerHTML;
+// const template = Handlebars.compile(source);
 
-const generatedHTML = template(data);
+// const generatedHTML = template(data);
 
-document.getElementById("contentContainer").innerHTML = generatedHTML;
+// document.getElementById("contentContainer").innerHTML = generatedHTML;
 
-document.getElementById("homeBtn").addEventListener("click", function () {
-  window.location.href = "/home";
-});
+// document.getElementById("homeBtn").addEventListener("click", function () {
+//   window.location.href = "/home";
+// });
 
-document.getElementById("logoutBtn").addEventListener("click", function () {
-  alert("Logged out!");
-});
+// document.getElementById("logoutBtn").addEventListener("click", function () {
+//   alert("Logged out!");
+// });
+
+const submitButton = document.querySelector('.create-character');
+
+const createBtnHandler = async (event) => {
+  event.preventDefault();
+
+  // Profile input values
+  const name = document.querySelector('#characterName').value.trim();
+  const image = document.querySelector('#')
+  
+}
