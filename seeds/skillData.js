508f19edf3e0ea3a9723ed00cf81d9c9390c2dde
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-08-15 21:20:55 -0500

    Redo of seeds to remove testing data and have placecard data for demo

--- seeds/skillData.js
+++ seeds/skillData.js
@@ -3,39 +3,27 @@ const { Skill } = require('../models');
 const skilldata = [
     {
         name: "Accio",
-        profile_id: 2
+        profile_id: 1
     },
     {
         name: "Arresto Momentum",
-        profile_id: 2
-    },
-    {
-        name: "Avada Kedavra",
         profile_id: 1
     },
     {
         name: "Alohomora",
-        profile_id: 2
-    },
-    {
-        name: "Bombarda",
-        profile_id: 1
-    },
-    {
-        name: "Confringo",
-        profile_id: 1
-    },
-    {
-        name: "Crucio",
         profile_id: 1
     },
     {
         name: "Descendo",
-        profile_id: 2
+        profile_id: 1
     },
     {
         name: "Polyjuice Potion",
-        profile_id: 2
+        profile_id: 1
+    },
+    {
+        name: "Patronus",
+        profile_id: 1
     }
 ]
 
--- seeds/statsData.js
+++ seeds/statsData.js
@@ -2,65 +2,35 @@ const { Stats } = require('../models');
 
 const statsdata =  [
     {
-        name: "Potions",
-        level: 5,
-        profile_id: 1
-    },
-    {
-        name: "Dark Arts",
-        level: 14,
-        profile_id: 1
-    },
-    {
-        name: "Herbology",
-        level: 2,
-        profile_id: 1
-    },
-    {
-        name: "Divination",
-        level: 0,
-        profile_id: 1
-    },
-    {
-        name: "Transfiguration",
-        level: 4,
-        profile_id: 1
-    },
-    {
-        name: "Astrology",
-        level: 7,
-        profile_id: 1
-    },
-    {
         name: "Charms",
         level: 12,
-        profile_id: 2
+        profile_id: 1
     },
     {
         name: "Defense Against the Dark Arts",
         level: 10,
-        profile_id: 2
+        profile_id: 1
     },
     {
         name: "Potions",
         level: 3,
-        profile_id: 2
+        profile_id: 1
     },
     {
         name: "Astrology",
         level: 8,
-        profile_id: 2
+        profile_id: 1
     },
     {
         name: "Herbology",
         level: 5,
-        profile_id: 2
+        profile_id: 1
     },
     {
         name: "Transfiguration",
         level: 6,
-        profile_id: 2
-    }
+        profile_id: 1
+    },
 ]
 
 const statsData = () => Stats.bulkCreate(statsdata);
