a5db0ae06fa2b75b5d7407de625065e1bbacab00
SkylarHammond55 <skylarhammond55@gmail.com>
2023-09-21 14:50:21 -0500

    added new hbs template with radar chart along with routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
 const createRoutes = require('./createRoutes')
+const infoRoutes = require('./infoRoutes');
 
 router.use('/users', userRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/create', createRoutes);
+router.use('/info', infoRoutes);
 
 module.exports = router;
--- controllers/api/infoRoutes.js
+++ controllers/api/infoRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { Stats } = require('../models');
 
-router.get('/info', function(req, res, next) {
-  res.render('info', { title: 'Information' });
+router.get('/info', async function(req, res, next) {
+  const statsData = await Stats.findAll({ where: { profile_id: 1 } });
+  const stats = statsData.map(stat => stat.get({ plain: true }));
+  res.render('info', { title: 'Information', stats });
 });
+
 
 module.exports = router;
